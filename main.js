9da78a04bfb68820042ecfaac68296aaa44736c4
cktang88 <arctangent20@yahoo.com>
2017-10-17 00:42:55 -0500

    Tiny refactoring

--- main.js
+++ main.js
@@ -1,63 +1,64 @@
-let sharp = require('sharp');
-var fs = require('fs');
+const sharp = require('sharp');
+const fs = require('fs');
 
-var walkPath = require('./config.js').basedir;
+let walkPath = require('./config.js').basedir;
 console.log("Starting...");
 
 // credit: https://gist.github.com/adamwdraper/4212319
-var walk = function (dir, done) {
-    fs.readdir(dir, function (error, list) {
-        if (error) {
-            return done(error);
+let walk = (dir, done) => {
+  fs.readdir(dir, function (error, list) {
+    if (error) {
+      return done(error);
+    }
+
+    let i = 0;
+
+    let next = () => {
+      let file = list[i++];
+
+      if (!file) {
+        return done(null);
+      }
+
+      file = dir + '/' + file;
+
+      fs.stat(file, (error, stat) => {
+
+        if (stat && stat.isDirectory()) {
+          // recurse down a directory
+          walk(file, (error) => {
+            next();
+          });
+        } else {
+          // do stuff to file here
+          console.log(file);
+
+          next();
         }
-
-        var i = 0;
-
-        (function next () {
-            var file = list[i++];
-
-            if (!file) {
-                return done(null);
-            }
-            
-            file = dir + '/' + file;
-            
-            fs.stat(file, function (error, stat) {
-        
-                if (stat && stat.isDirectory()) {
-                    walk(file, function (error) {
-                        next();
-                    });
-                } else {
-                    // do stuff to file here
-                    console.log(file);
-
-
-                    next();
-                }
-            });
-        })();
-    });
+      });
+    }
+    next();
+  });
 };
 
 // optional command line params
 //      source for walk path
 process.argv.forEach(function (val, index, array) {
-    if (val.indexOf('source') !== -1) {
-        walkPath = val.split('=')[1];
-    }
+  if (val.indexOf('source') !== -1) {
+    walkPath = val.split('=')[1];
+  }
 });
 
 console.log('-------------------------------------------------------------');
 console.log('processing...');
 console.log('-------------------------------------------------------------');
 
-walk(walkPath, function(error) {
-    if (error) {
-        throw error;
-    } else {
-        console.log('-------------------------------------------------------------');
-        console.log('finished.');
-        console.log('-------------------------------------------------------------');
-    }
+walk(walkPath, function (error) {
+  if (error) {
+    throw error;
+  } else {
+    console.log('-------------------------------------------------------------');
+    console.log('finished.');
+    console.log('-------------------------------------------------------------');
+  }
 });
