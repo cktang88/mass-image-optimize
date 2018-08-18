094ff2fee3f6d284b6d7e935e0eba665b54516fd
cktang88 <arctangent20@yahoo.com>
2018-08-18 16:13:14 -0500

    Lint

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-'use strict'
-import * as sharp from 'sharp';
-import * as fs from 'fs';
-import * as sizeOf from 'image-size';
-import * as program from 'commander';
+"use strict";
+import * as sharp from "sharp";
+import * as fs from "fs";
+import * as sizeOf from "image-size";
+import * as program from "commander";
 
 // defaults
 let max_width = 1200;
@@ -13,12 +13,17 @@ let basedir = process.cwd();
 
 // TODO: parse command line options with commander...
 program
-  .option('-d, --dir <path>',
-    'base directory containing unoptimized files')
-  .option('-w, --max_width <n>',
-    'max width of new image (will also scale height, maintains aspect ratio', parseInt)
-  .option('-h, --max_height <n>',
-    'max height of new image (will also scale width, maintains aspect ratio', parseInt)
+  .option("-d, --dir <path>", "base directory containing unoptimized files")
+  .option(
+    "-w, --max_width <n>",
+    "max width of new image (will also scale height, maintains aspect ratio",
+    parseInt
+  )
+  .option(
+    "-h, --max_height <n>",
+    "max height of new image (will also scale width, maintains aspect ratio",
+    parseInt
+  )
   .parse(process.argv);
 
 // if program was called with no arguments, show help. (or use defaults?)
@@ -35,7 +40,7 @@ process.exit();
 // credit: https://gist.github.com/adamwdraper/4212319
 let walk = (dir: string, done: any) => {
   // make new dir if doesn't exist
-  let newdir = dir.replace(basedir, basedir + '-optimized');
+  let newdir = dir.replace(basedir, basedir + "-optimized");
   if (!fs.existsSync(newdir)) {
     fs.mkdirSync(newdir);
   }
@@ -47,36 +52,32 @@ let walk = (dir: string, done: any) => {
     let i = 0;
     let next = () => {
       let filename = list[i++];
-      if (!filename)
-        return done(null);
+      if (!filename) return done(null);
 
-      let filepath = dir + '/' + filename;
+      let filepath = dir + "/" + filename;
       fs.stat(filepath, (err, stat: fs.Stats) => {
         if (stat && stat.isDirectory()) {
           // recurse down a directory
-          walk(filepath, (err) => {
+          walk(filepath, err => {
             next();
           });
         } else {
           // process new file
-          let newpath = newdir + '/' + filename;
+          let newpath = newdir + "/" + filename;
           processFile(filepath, newpath);
           next();
         }
       });
-    }
+    };
     next();
   });
 };
 
-let processFile = (file: string, newpath :string) => {
+let processFile = (file: string, newpath: string) => {
   console.log(file);
   // if image, optimize
-  if (file.endsWith('.jpg') || file.endsWith('.png')) {
-    const {
-      width,
-      height
-    } = sizeOf(file);
+  if (file.endsWith(".jpg") || file.endsWith(".png")) {
+    const { width, height } = sizeOf(file);
     if (width < max_width && height < max_height) {
       return;
     }
@@ -85,28 +86,29 @@ let processFile = (file: string, newpath :string) => {
       .max()
       .rotate()
       .toFile(newpath, (err, info) => {
-        if (err)
-          console.log(err);
-      })
+        if (err) console.log(err);
+      });
   }
   // else just copy
   else {
-    fs.createReadStream(file)
-    .pipe(fs.createWriteStream(newpath)
-  );
+    fs.createReadStream(file).pipe(fs.createWriteStream(newpath));
   }
-}
+};
 
-console.log('-------------------------------------------------------------');
-console.log('Processing...');
-console.log('-------------------------------------------------------------');
+console.log("-------------------------------------------------------------");
+console.log("Processing...");
+console.log("-------------------------------------------------------------");
 
-walk(basedir, (err) => {
+walk(basedir, err => {
   if (err) {
     throw err;
   } else {
-    console.log('-------------------------------------------------------------');
-    console.log('Finished.');
-    console.log('-------------------------------------------------------------');
+    console.log(
+      "-------------------------------------------------------------"
+    );
+    console.log("Finished.");
+    console.log(
+      "-------------------------------------------------------------"
+    );
   }
 });
